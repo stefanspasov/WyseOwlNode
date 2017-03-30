8ead3bc82c14baff9ee98702fe57ffa8ac2893ba
Stefan Spasov <stespa@jetshop.se>
2017-03-30 17:24:59 +0200

    dispatcher

--- server.js
+++ server.js
@@ -2,38 +2,32 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var http = require('http');
+var HttpDispatcher = require('httpdispatcher');
+var dispatcher     = new HttpDispatcher();
 var port = process.env.PORT || 8081;        // set our port
-var router = express.Router();
 
-// middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Get request incoming.');
-    next(); // make sure we go to the next routes and don't stop here
+function handleRequest(request, response){
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+dispatcher.onGet("/page1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page One');
+});    
+
+dispatcher.onGet("/page2", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page TWO');
 });
-
-// test route to make sure everything is working (accessed at GET http://localhost:8081/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'it works!' });   
-});
-
-router.route('/calc/:calc_1/:calc_2')
-.get(function(req, res) {
-       res.json("hello" + req.params.calc_1 + req.params.calc_2);
-    });
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
 
 
 // START THE SERVER
 // =============================================================================
-http.createServer(function (req, res) {
-    //res.writeHead(200, { 'Content-Type': 'text/plain' });
-    //res.end('Hello World\n');
-}).listen(port);
-//app.listen(port);
-//console.log('Magic happens on port ' + port);
+http.createServer(handleRequest).listen(port);
